6eba82c49f9471d1e3e6589571d3e14afb6dfd33
phanhphanh235 <phamphuonganhk23@gmail.com>
2024-05-08 19:43:46 +0700

    project

---
+++ ConsoleApp1/Bai1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Bai1
+    {
+        static void Main(string[] args)
+        {
+            int a = 12345;
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int a;
+            Console.WriteLine("Input a:");
+            a = int.Parse(Console.ReadLine());
+            double b;
+            Console.WriteLine("Input b:");
+            b = Convert.ToSingle(Console.ReadLine());
+            double c;
+            Console.WriteLine("Input c:");
+            double.TryParse(Console.ReadLine(), out c);
+
+            Console.WriteLine("c = ", +c);
+            Console.WriteLine("c = {0}", c);
+            Console.WriteLine($"c ={c}")
+        }
+    }
+}
